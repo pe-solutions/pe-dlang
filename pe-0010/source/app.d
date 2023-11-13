ca0c733f69a5c248d232d9d0a9be470daf7ab1ce
Menjanahary <menjaraz@gmail.com>
2023-11-13 14:21:11 +0300

    Add Project Euler problem 10 Solution

---
+++ pe-0010/source/app.d
@@ -0,0 +1,44 @@
+// Summation of Primes
+// https://projecteuler.net/problem=10
+
+import std.stdio : writefln;
+import std.datetime.stopwatch: StopWatch;
+import std.range: iota;
+import std.algorithm: filter, map, sum;
+
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    if (n <= 3) {
+        return true;
+    }
+    if (n % 2 == 0 || n % 3 == 0) {
+        return false;
+    }
+    
+    int i = 5;
+    while (i * i <= n) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return false;
+        }
+        i += 6;
+    }
+    
+    return true;
+}
+
+void main() {
+    StopWatch timer;
+    timer.start();
+    
+     auto answer = iota(2, 2_000_000)
+                .filter!(isPrime)
+                .map!(a => cast(ulong)a)
+                .sum;
+    
+    timer.stop();
+    
+    writefln("\nProject Euler #10\nAnswer: %s", answer);
+    writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
+}
