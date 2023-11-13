2a89cbb3e0070329d8565f2de6fea428c52d40f7
Menjanahary <menjaraz@gmail.com>
2023-11-13 16:01:15 +0300

    Add Project Euler problem 206 Solution
    Concealed Square

---
+++ pe-0206/source/app.d
@@ -0,0 +1,53 @@
+// Concealed Square
+// https://projecteuler.net/problem=206
+
+import std.stdio : writefln;
+import std.datetime.stopwatch: StopWatch;
+import std.math: sqrt;
+import std.range: iota;
+import std.algorithm: filter, sum;
+import std.numeric: gcd;
+
+ulong calculateSquareRootUpperBound() {
+    return (cast(ulong)sqrt(cast(double)1929394959697989990uL) / 10) * 10;
+}
+
+ulong calculateSquareRootLowerBound() {
+    return (cast(ulong)sqrt(cast(double)1020304050607080900uL) / 10) * 10;
+}
+
+bool hasConcealedSquarePattern(ulong candidate) {
+    candidate /= 100;
+
+    for (ulong digit = 9; digit > 0; digit--, candidate /= 100) {
+        if (candidate % 10 != digit) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void main() {
+    StopWatch timer;
+    timer.start();
+    
+    auto max = calculateSquareRootUpperBound();
+    auto min = calculateSquareRootLowerBound();
+
+    ulong answer= 0;
+
+    for (ulong i = min; i <= max; i += 10) {
+        auto candidate = i * i;
+
+        if (hasConcealedSquarePattern(candidate)) {
+            answer = i; // writeln(i);
+            break;
+        }
+    }
+        
+    timer.stop();
+        
+    writefln("\nProject Euler #206\nAnswer: %s", answer);
+    writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
+}
