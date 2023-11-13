474beba672f3e82a6b9124cb0d6bd3507200a5f8
Menjanahary <menjaraz@gmail.com>
2023-11-13 17:02:36 +0300

    Add Project Euler problem 47 solution
    Distinct Primes Factors

---
+++ pe-0047/source/app.d
@@ -0,0 +1,59 @@
+import std.stdio;
+import std.datetime.stopwatch;
+import std.container;
+import std.algorithm;
+
+ulong[] generateOmegaSieve(ulong limit, ulong requiredFactors) {
+    // The Sieve!
+    
+	ulong[] omegaSieve = new ulong[](limit);
+
+    for (ulong i = 2; i < limit; i++) {
+        if (omegaSieve[i] == 0) {
+            for (ulong j = i; j < limit; j += i) {
+                omegaSieve[j]++;
+            }
+        }
+    }
+    
+    return omegaSieve;
+}
+
+ulong findFirstNumber() {
+    // Sieve approach... neither BF nor Memoization is worth it!
+	
+	ulong limit = 135_000; // ...just above the correct answer ;-)
+	
+    ulong requiredFactors = 4; // Required number of distinct prime factors
+
+    auto omegaSieve = generateOmegaSieve(limit, requiredFactors);
+
+    for (ulong i = 1; i < limit - requiredFactors; i++) {
+        bool found = true;
+        
+        for (ulong j = 0; j < requiredFactors; j++) {
+            if (omegaSieve[i + j] != requiredFactors) {
+                found = false;
+                break;
+            }
+        }
+
+        if (found) {
+            return i;
+        }
+    }
+
+    return 0; // If no such number is found
+}
+
+void main() {
+    StopWatch timer;
+    timer.start();
+
+    auto answer = findFirstNumber();
+
+    timer.stop();
+    
+    writefln("\nProject Euler #47\nAnswer: %s", answer);
+    writefln("Elapsed time: %s milliseconds.", timer.peek.total!"msecs"());
+}
