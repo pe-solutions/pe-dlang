7ff1d405a3ea32e80e3a337dbfc6ef409902b548
Menjanahary <menjaraz@gmail.com>
2023-11-13 12:01:55 +0300

    Add Project Euler problem 6 Solution

---
+++ pe-0006/source/app.d
@@ -0,0 +1,19 @@
+// Sum Square Difference
+// https://projecteuler.net/problem=6
+
+import std.stdio : writefln;
+import std.datetime.stopwatch: StopWatch;
+import std.range: iota;
+import std.algorithm: map, sum;
+
+void main() {
+    StopWatch timer;
+    timer.start();
+    
+    auto answer = iota(1, 100+1).sum ^^ 2 - iota(1, 100+1).map!(a => a ^^ 2).sum;
+    
+    timer.stop();
+    
+    writefln("\nProject Euler #6\nAnswer: %s", answer);
+    writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
+}
