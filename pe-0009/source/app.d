ea7f57b71cd485ac54403edf0cdd7009114f1795
Menjanahary <menjaraz@gmail.com>
2023-11-15 12:58:49 +0400

    Update app.d
    Replace with better and efficient algorithm: Brute force Diophantine equation solver

--- pe-0009/source/app.d
+++ pe-0009/source/app.d
@@ -1,63 +1,36 @@
 // Special Pythagorean Triplet
 // https://projecteuler.net/problem=9
 
-import std.stdio : writefln;
+import std.stdio : writeln, writefln;
 import std.datetime.stopwatch: StopWatch;
-import std.math: abs;
-import std.conv: to;
+import std.typecons: tuple, Tuple;
 
-double solveEquation(double a) {
-    // Function to solve
-    double b = 1000 * (500 - a) / (1000 - a);
-
-    return a * a + b * b - (1000 - a - b) * (1000 - a - b);
-}
-
-double solveDerivative(double a) {
-    // Derivative of the function to solve
-    double b = 1000 * (500 - a) / (1000 - a);
-
-    double da = 2 * a - 2 * (1000 - a - b);
-    double db = 2 * b - 2 * (1000 - a - b);
-    
-    return (a * da + b * db);
-}
-
-double newtonRaphson(double initialGuess, int maxIterations = 100, double tolerance = 1e-6) {
-    // Numerical solver
-    
-    double x = initialGuess;
-
-    for (int i = 0; i < maxIterations; ++i) {
-        double fx = solveEquation(x);
-        double dfx = solveDerivative(x);
-
-        double deltaX = fx / dfx;
-
-        x -= deltaX;
-
-        if (abs(deltaX) < tolerance) {
-            return x;
+Tuple!(uint, uint, uint) findTriplet() {
+    for (uint a = 1; a < 334; a++) {
+        for (uint b = a + 1; b < (1000 - a) / 2; b++) {
+            auto c = 1000 - a - b;
+            //
+            if (a * a + b * b == c * c) {
+                return tuple(a, b, c);
+            }
         }
     }
-    
-    return double.nan;
+
+    return tuple(0u, 0u, 0u); // No solution found
 }
 
 void main() {
     StopWatch timer;
     timer.start();
-    
-    // Starting with an initial guess of 200
-    long a = newtonRaphson(200).to!long;
 
-    long b = 1000 * (500 - a) / (1000 - a);
-    long c = 1000 - a - b;
+    auto result = findTriplet();
 
-    auto answer = a*b*c;
+    if (result[0] != 0) {
+        writefln("\nProject Euler #9\nAnswer: %s", result[0] * result[1] * result[2]);
+    } else {
+        writefln("No solution found.");
+    }
 
     timer.stop();
-    
-    writefln("\nProject Euler #9\nAnswer: %s", answer);
     writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
 }
