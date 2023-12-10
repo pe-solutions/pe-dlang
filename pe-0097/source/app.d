21981a1218398b54508561b883a1a56e4a82816d
Menjanahary <menjaraz@gmail.com>
2023-12-10 16:03:47 +0300

    Add Project Euler problem 97 solution

---
+++ pe-0097/source/app.d
@@ -0,0 +1,22 @@
+// Large Non-Mersenne Prime
+// https://projecteuler.net/problem=97
+
+import std.bigint;
+import std.datetime.stopwatch: StopWatch;
+import std.stdio;
+
+long large_non_mersenne_prime(BigInt base, BigInt exponent, BigInt coeff, BigInt modulus) {
+    return powmod((coeff * powmod(base, exponent, modulus) + 1), 1.BigInt, modulus).toLong;
+}
+
+void main() {
+    StopWatch timer;
+    timer.start();
+    
+    auto answer = large_non_mersenne_prime(2.BigInt, 7_830_457.BigInt, 28_433.BigInt, 10.BigInt ^^ 10);
+
+    timer.stop();
+
+    writefln("\nProject Euler #97\nAnswer: %s", answer);
+    writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
+}
