d6cba4b6a72c0757c5c6775544835a8cf1197f32
Menjanahary <menjaraz@gmail.com>
2023-12-29 11:39:49 +0400

    Refactor app.d
    Bump to a more functional version

--- pe-0048/source/app.d
+++ pe-0048/source/app.d
@@ -1,37 +1,21 @@
 // Self Powers
 // https://projecteuler.net/problem=48
 
-import std.stdio;
+import std.stdio : writefln;
 import std.datetime.stopwatch: StopWatch;
+import std.bigint : BigInt, powmod;
+import std.range : iota;
+import std.algorithm: map, sum;
 
-ulong sum_of_self_power(ulong n, ulong modulus) {
-    ulong result = 0;
-
-    for (int i = 1; i <= n; ++i) {
-        ulong f = i;
-
-        for (int j = 1; j < i; ++j) {
-            f = (f * i) % modulus;
-        }
-
-        result = (result + f) % modulus;
-    }
-
-    return result;
-}
-
-void main() {
+void main()
+{
     StopWatch timer;
     timer.start();
     
-    const ulong N = 1000uL;
-    const ulong MOD = 10uL^^10;
-
-    ulong answer = sum_of_self_power(N, MOD);
-
+    auto answer = powmod(iota(1, 1001).map!(i => BigInt(i) ^^ i).sum, 1.BigInt, 10.BigInt ^^ 10);
+    
     timer.stop();
 
     writefln("\nProject Euler #48\nAnswer: %s", answer);
     writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
 }
-
