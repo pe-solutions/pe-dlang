8818c62cce0e836c8716983dd99e800727a480ec
Menjanahary <menjaraz@gmail.com>
2024-01-30 22:25:11 +0400

    Update app.d
    Refactoring. Closed form to generate the sequence straight forward.

--- pe-0002/source/app.d
+++ pe-0002/source/app.d
@@ -1,27 +1,32 @@
 // Even Fibonacci Numbers
 // https://projecteuler.net/problem=2
 
-import std.stdio : writefln;
+import std.stdio: writefln;
 import std.datetime.stopwatch: StopWatch;
-import std.range : recurrence;
+import std.range: recurrence;
 import std.algorithm.searching : until;
-import std.algorithm : filter, sum;
+import std.algorithm: sum;
+
+ulong calculateSumOfEvenFibo(ulong limit)
+{
+    auto evenNumberedTermFib = recurrence!("4*a[n-1] + a[n-2]")(2uL, 8uL);
+    
+    return evenNumberedTermFib
+        .until!(x => x >= limit)
+        .sum;
+}
 
 void main()
 {
     StopWatch timer;
     timer.start();
     
-    auto fib = recurrence!("a[n-1] + a[n-2]")(1uL, 1uL);
+    const ulong UPPER_LIMIT = 4_000_000uL;
     
-    auto answer = fib
-        .until!(x => x >= 4_000_000uL)
-        .filter!(x => x % 2uL == 0uL)
-        .sum;
-
+    auto answer = calculateSumOfEvenFibo(UPPER_LIMIT);
+    
     timer.stop();
-        
+    
     writefln("\nProject Euler #2\nAnswer: %s", answer);
     writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
-
 }
