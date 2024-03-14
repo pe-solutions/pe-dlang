eb6ef11d94fc4da09e267a32980dbf0c71e45e75
Menjanahary <menjaraz@gmail.com>
2024-03-14 09:01:39 +0400

    Update app.d

--- pe-0040/source/app.d
+++ pe-0040/source/app.d
@@ -20,8 +20,8 @@ void main() {
     
     auto answer = c[0] * c[9] * c[99] * c[999] * c[9_999] * c[99_999] * c[999_999];
 	
-	timer.stop();
+    timer.stop();
         
-        writefln("\nProject Euler #40\nAnswer: %s", answer);
-	writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
+    writefln("\nProject Euler #40\nAnswer: %s", answer);
+    writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
 }
