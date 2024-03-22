53e13e72220437569aaa562b74079b2195f4d4b2
Menjanahary <menjaraz@gmail.com>
2024-03-22 16:07:11 +0400

    Update app.d
    Refactor to a simple algorithm

--- pe-0076/source/app.d
+++ pe-0076/source/app.d
@@ -4,67 +4,31 @@
 import std.stdio;
 import std.datetime.stopwatch: StopWatch;
 
-long numbpart(long n)
-{
-    long[] partitionTable = new long[n + 1];
-    const long[] initialVector = [1, 1, 2, 3];
+size_t computePartitions(int n) {
+    auto partitions = new size_t[n + 1];
+    partitions[0] = 1;
 
-    void initializePartitionTable(const long[] initialValue)
-    {
-        foreach (i, value; initialValue)
-            partitionTable[i] = value;
-    }
-
-    long calculatePartition(long number)
-    {
-        long sum = 0;
-        long k = 1;
-        int sign = 1;
-
-        do
-        {
-            long pentagonal1 = k * (3 * k - 1) / 2;
-            long pentagonal2 = k * (3 * k + 1) / 2;
-
-            if (pentagonal1 > number)
-                break;
-
-            if (number - pentagonal1 >= 0)
-                sum += partitionTable[number - pentagonal1] * sign;
-
-            if (number - pentagonal2 >= 0)
-                sum += partitionTable[number - pentagonal2] * sign;
-
-            k++;
-            sign = -sign;
-        } while (true);
-
-        return sum;
-    }
- 
-	initializePartitionTable(initialVector);
-
-    long currentNumber = initialVector.length;
-    do
-    {
-        partitionTable[currentNumber] = calculatePartition(currentNumber);
-
-        if (currentNumber == n)
-        {
-            return partitionTable[currentNumber];
+    foreach (i; 1 .. n + 1) {
+        foreach (j; i .. n + 1) {
+            partitions[j] += partitions[j - i];
         }
+    }
 
-        currentNumber++;
-    } while (true);
+    return partitions[n];
 }
 
-void main()
-{
+size_t nPartitions(int n) {
+    return computePartitions(n) - 1;
+}
+
+
+void main() {
     StopWatch timer;
     timer.start();
-
-    auto answer = numbpart(100) - 1;
-
+    
+    const int N = 100;
+    auto answer = nPartitions(N);
+    
     timer.stop();
 
     writefln("\nProject Euler #76\nAnswer: %s", answer);
