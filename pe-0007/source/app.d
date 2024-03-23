f3000a4ba6e9bc9f5047ff527b0f05a59f22c5b5
Menjanahary <menjaraz@gmail.com>
2024-03-23 20:41:25 +0400

    Update app.d
    Refactoring

--- pe-0007/source/app.d
+++ pe-0007/source/app.d
@@ -1,49 +1,38 @@
 // 10001st Prime
 // https://projecteuler.net/problem=7
 
-import std.stdio : writefln;
+import std.stdio;
 import std.datetime.stopwatch: StopWatch;
 
-bool isPrime(int n) {
-    if (n <= 1) {
-        return false;
-    }
-    
-    if (n <= 3) {
-        return true;
-    }
-    
-    if (n % 2 == 0 || n % 3 == 0) {
-        return false;
-    }
-    
-    for (int i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0) {
-            return false;
-        }
-    }
-    
-    return true;
-}
-
 int nthPrime(int n) {
+    if (n <= 0) {
+        return -1;
+    }
+    
     if (n == 1) {
         return 2;
     }
     
-    int primeCount = 1; // Counts the prime numbers found so far
-    int candidate = 3;  // Start with the first odd number after 2
+    int primeCount = 1;
+    int candidate = 3;
     
-    for(;;) {
-        if (isPrime(candidate)) {
+    for( ; ; candidate += 2) {
+        bool isPrime = true;
+        
+        for (int i = 3; i * i <= candidate; i += 2) {
+            if (candidate % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        
+        if (isPrime) {
             primeCount++;
             
             if (primeCount == n) {
                 return candidate;
             }
         }
-        
-        candidate += 2; // Move to the next odd number
     }
 }
 
@@ -51,7 +40,9 @@ void main() {
     StopWatch timer;
     timer.start();
     
-    auto answer = nthPrime(10_001);
+    const int N = 10_001;
+    
+    auto answer = nthPrime(N);
     
     timer.stop();
     
