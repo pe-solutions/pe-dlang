2ebedb0ac2b1a3527379201814de088d9ae8d680
Menjanahary <menjaraz@gmail.com>
2024-03-23 18:56:50 +0300

    Add Project Euler problem 15 solution
    Concise code using FP paradigm 🦾🦾🦾

---
+++ pe-0015/source/app.d
@@ -0,0 +1,22 @@
+// Lattice Paths
+// https://projecteuler.net/problem=15
+
+import std.stdio: writefln;
+import std.datetime.stopwatch: StopWatch;
+import std.range: iota;
+import std.algorithm.iteration: reduce;
+
+void main() {
+    StopWatch timer;
+    timer.start();
+    
+    alias countRoutes = (int n) => reduce!((a, b) => a * (n + b) / b)(1L, iota(1, n + 1));
+    
+    const int N = 20;
+    auto answer = countRoutes(N);
+    
+    timer.stop();
+    
+    writefln("\nProject Euler #15\nAnswer: %s", answer);
+    writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
+}
