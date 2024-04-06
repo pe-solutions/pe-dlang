415b232c3cb8c6c7e0047a7ca6d66e30d0ca44f0
Menjanahary <menjaraz@gmail.com>
2024-04-06 12:35:48 +0300

    Add Project Euler problem 28 Solution

---
+++ pe-0028/source/app.d
@@ -0,0 +1,25 @@
+// Number Spiral Diagonals
+// https://projecteuler.net/problem=28
+
+import std.stdio;
+import std.datetime.stopwatch: StopWatch;
+
+/// Closed form
+long sumOfSpiralDiagonals(const long n) =>
+    (4L * n ^^ 3L + 3L * n ^^ 2L + 8L * n - 9L) / 6L;
+
+void main()
+{
+    StopWatch timer;
+    timer.start();
+
+    // Size of the spiral
+    const SIZE = 1_001L;
+    
+    auto answer = sumOfSpiralDiagonals(SIZE);
+    
+    timer.stop();
+    
+    writefln("\nProject Euler #28\nAnswer: %s", answer);
+    writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
+}
