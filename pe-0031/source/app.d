224440e4d8da8a1bf79abe61f9d623108d71182e
Menjanahary <menjaraz@gmail.com>
2024-04-11 22:23:13 +0300

    Add Project Euler problem 31 Solution

---
+++ pe-0031/source/app.d
@@ -0,0 +1,35 @@
+// Coin Sums
+// https://projecteuler.net/problem=31
+
+import std.stdio;
+import std.datetime.stopwatch: StopWatch;
+
+void main()
+{
+    StopWatch timer;
+    timer.start();
+
+    // Dynamic Programming
+    
+    const TARGET = 200;
+    
+    int[] coinValues = [1, 2, 5, 10, 20, 50, 100, 200];
+    int[] ways = new int[TARGET + 1];
+    
+    ways[0] = 1;
+
+    foreach (int coin; coinValues)
+    {
+        for (int i = coin; i <= TARGET; i++)
+        {
+            ways[i] += ways[i - coin];
+        }
+    }
+    
+    auto answer = ways[TARGET];
+    
+    timer.stop();
+
+    writefln("\nProject Euler #31\nAnswer: %s", answer);
+    writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
+}
