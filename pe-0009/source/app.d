33d194227cd4844aa6c07650291b094f494361da
Menjanahary <menjaraz@gmail.com>
2024-04-11 16:04:39 +0400

    Refactor app.d
    Using meaningful constant/variable/function name. Simplify algorithm. More rigorious test for edge case "no solution".

--- pe-0009/source/app.d
+++ pe-0009/source/app.d
@@ -5,25 +5,18 @@ import std.stdio : writeln, writefln;
 import std.datetime.stopwatch: StopWatch;
 import std.typecons: tuple, Tuple;
 
-const P = 1_000u;
+const PERIMETER = 1_000u;
 
 Tuple!(uint, uint, uint) findTriplet() {
-    for (uint a = 1; a < P / 3; a++) {
-        // Calculate numerator (n) and denominator (d)
-        int n = P^^2 - 2 * P * a;
-        int d = 2 * P - 2 * a;
-
-        // Check if n is divisible evenly by d
-        if (d != 0 && n % d == 0)
-        {
-            // Calculate b and c based on conditions
-            uint b = n / d;
-            uint c = P - a - b;
-
-            return tuple(a, b, c);
+    foreach (uint a; 1 .. PERIMETER/3) {
+        foreach (uint b; a + 1 .. PERIMETER/2) {
+            uint c = PERIMETER - a - b;
+            //
+            if (a * a + b * b == c * c)
+                return tuple(a, b, c);
         }
     }
-
+    //
     return tuple(0u, 0u, 0u); // No solution found
 }
 
@@ -33,7 +26,7 @@ void main() {
 
     auto result = findTriplet();
 
-    if (result[0] != 0) {
+    if (result[0] != 0 && result[1] != 0 && result[2] != 0) {
         writefln("\nProject Euler #9\nAnswer: %s", result[0] * result[1] * result[2]);
     } else {
         writefln("No solution found.");
@@ -42,4 +35,3 @@ void main() {
     timer.stop();
     writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());
 }
-
