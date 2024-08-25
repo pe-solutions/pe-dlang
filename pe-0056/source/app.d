aaa24572210378a385c9f3d10345b5c1022f9aaf
Menjanahary <menjaraz@gmail.com>
2024-08-25 14:16:39 +0400

    Update app.d
    Refactor toward a concise more easy to understand D code

--- pe-0056/source/app.d
+++ pe-0056/source/app.d
@@ -1,32 +1,20 @@
-// Powerful Digit Sum
-// https://projecteuler.net/problem=56
-
 import std.stdio;
 import std.datetime.stopwatch;
-import std.range;
-import std.bigint;
 import std.algorithm;
+import std.bigint;
+import std.range;
 import std.conv;
 
 void main() {
     auto timer = StopWatch(AutoStart.yes);
     
-    // Generate range of values for a and b
-    auto rangeA = iota(1, 100);
-    auto rangeB = iota(1, 100);
-
-    // Compute maximum digital sum
-    auto maxDigitalSum = rangeA
-        .cartesianProduct(rangeB)       // Generate all pairs (a, b)
-        .map!(ab => BigInt(ab[0])^^ab[1])  // Calculate a^b for each pair
-        .map!(n => n.to!string          // Convert to string
-                      .map!(c => c-'0') // Convert digits to integers
-                      .reduce!"a + b")  // Sum the digits
-        
-        .reduce!"max(a, b)";            // Find the maximum digital sum
-
-    timer.stop();
+    "\nProject Euler #56\nAnswer: %s".writefln(iota(1, 100)
+        .cartesianProduct(iota(1, 100))
+        .map!(ab => BigInt(ab[0])^^ab[1])
+        .map!(n => n.to!string.map!(c => c - '0').sum)
+        .maxElement);
     
-    writefln("\nProject Euler #56\nAnswer: %s", maxDigitalSum);
-    writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"());    
+    timer.stop;
+    
+    writefln("Elapsed time: %s milliseconds.\n", timer.peek.total!"msecs"()); 
 }
